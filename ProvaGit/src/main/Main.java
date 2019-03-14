8e42ae4b060efe72ea40b3a37337bb2944bf6b55
Michele Mosca <michele.mosca026@gmail.com>
2019-03-14 19:40:36 +0100

    Risposta al commento

--- ProvaGit/src/main/Main.java
+++ ProvaGit/src/main/Main.java
@@ -9,6 +9,8 @@ public class Main
 		System.out.println("Si");	//Commento aggiunto
 		System.out.println("Prova merge delle modifiche");	//Merge riuscito
 		System.out.println("Ti voglio bene, ma ho sonno."); //14.03.2019 16.13h
+		System.out.println("No problem"); //Non c'è bisogno che metti la data e l'ora, vedo tutto nella history
+		//Hai committato alle 16.16h comunque :)
 	}
 
 }
