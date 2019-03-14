cb6a6838ad9f351fde996c257ae301acc6cf1393
gianl <gianl@LAPTOP-9DE80S2M>
2019-03-14 16:16:42 +0100

    Ho aggiunto una cazzata

--- ProvaGit/src/main/Main.java
+++ ProvaGit/src/main/Main.java
@@ -8,7 +8,7 @@ public class Main
 		System.out.println("Ciao Gianluca, riesci a modificare questo messaggio?");
 		System.out.println("Si");	//Commento aggiunto
 		System.out.println("Prova merge delle modifiche");	//Merge riuscito
-		
+		System.out.println("Ti voglio bene, ma ho sonno."); //14.03.2019 16.13h
 	}
 
 }
