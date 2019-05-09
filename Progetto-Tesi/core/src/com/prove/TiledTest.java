73a27b1d723008337b9645a036f6b120fe5f5094
Michele Mosca <michele.mosca026@gmail.com>
2019-05-09 21:28:43 +0200

    Collisioni e gravità temporanea

--- Progetto-Tesi/core/src/com/prove/TiledTest.java
+++ Progetto-Tesi/core/src/com/prove/TiledTest.java
@@ -6,10 +6,15 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.game.graphics.CameraObject;
 
 /**
@@ -20,24 +25,52 @@ import com.game.graphics.CameraObject;
 
 public class TiledTest extends ApplicationAdapter implements InputProcessor 
 {
-    Texture img;
+	SpriteBatch batch;
+	Sprite sprite;
+	
+	Rectangle spawnPoint, ground;
+	float position;
+	
     TiledMap tiledMap;
     CameraObject cameraObject;
     TiledMapRenderer tiledMapRenderer;
+    
     boolean keyLeftPressed, keyRightPressed;
     
     @Override
     public void create () 
     {
-        keyLeftPressed = false;
-        keyRightPressed = false;
-
         tiledMap = new TmxMapLoader().load("assets/mappa.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
         cameraObject = new CameraObject(tiledMap);
+        batch = new SpriteBatch();
+        sprite = new Sprite(new Texture("assets/idle_right.gif"));
+        
+        keyLeftPressed = false;
+        keyRightPressed = false;
+        
         Gdx.input.setInputProcessor(this);
+        
+        for (MapObject obj : tiledMap.getLayers().get(1).getObjects().getByType(RectangleMapObject.class))
+        {
+        	spawnPoint = ((RectangleMapObject) obj).getRectangle();
+        }
+        
+        for (MapObject obj : tiledMap.getLayers().get(2).getObjects().getByType(RectangleMapObject.class))
+        {
+        	ground = ((RectangleMapObject) obj).getRectangle();
+        }
+        
+        position = spawnPoint.y;
+        System.out.println(position + " | " + ground.y);
     }
 
+    public void update()
+    {
+    	if (position > ground.y + ground.height)
+    		position -= 2;			//Fa ancora schifo adesso sistemo tutto
+    }
+    
     @Override
     public void render () 
     {
@@ -51,12 +84,28 @@ public class TiledTest extends ApplicationAdapter implements InputProcessor
         	cameraObject.moveRight();
         
         cameraObject.update();
-        System.out.println(cameraObject.getCamera().position.x + " " + cameraObject.getCamera().position.y);
         tiledMapRenderer.setView(cameraObject.getCamera());
         tiledMapRenderer.render();
+        
+        update();
+        
+        batch.begin();
+        
+        batch.draw(sprite, spawnPoint.x, position);
+        
+        batch.end();
     }
 
     @Override
+	public void dispose()
+	{
+		super.dispose();
+		batch.dispose();
+		tiledMap.dispose();
+		sprite.getTexture().dispose();
+	}
+
+	@Override
 	public void resize(int width, int height)
 	{
 		super.resize(width, height);
