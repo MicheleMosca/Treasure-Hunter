5612d7f54ac7f1024d31cdae6837d740f3d326c1
Michele Mosca <michele.mosca026@gmail.com>
2019-05-14 22:54:16 +0200

    Disabilito di default il full screen

--- Progetto-Tesi/core/src/com/game/AdventureGame.java
+++ Progetto-Tesi/core/src/com/game/AdventureGame.java
@@ -15,7 +15,7 @@ public class AdventureGame extends Game
 	public static final int worldWidth = 1280;
 	public static final int worldHeight = 720;
 	public static final float pixelPerMeter = 100;
-	public static final boolean fullScreenOnStart = true;
+	public static final boolean fullScreenOnStart = false;
 	
 	public SpriteBatch batch;
 	
