6a60df1a36a7de671e892a3970b79ecfefdd536a
Michele Mosca <michele.mosca026@gmail.com>
2019-05-15 10:59:00 +0200

    prova push

--- Progetto-Tesi/core/src/com/game/screens/PlayScreen.java
+++ Progetto-Tesi/core/src/com/game/screens/PlayScreen.java
@@ -40,7 +40,9 @@ public class PlayScreen implements Screen
 	public PlayScreen(AdventureGame game)
 	{
 		this.game = game;
-		
+
+		// prova
+
 		// Carico la mappa
 		TmxMapLoader mapLoader = new TmxMapLoader();
 		tiledMap = mapLoader.load("maps/level1/level1.tmx");
