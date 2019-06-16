f773f5db9994756cd4dafcd0fc4b55138203dce1
GianlucaC98 <gianluca.c98@outlook.it>
2019-06-16 11:20:20 +0200

    menu terminato

--- Progetto-Tesi/core/src/com/game/screens/MainMenuScreen.java
+++ Progetto-Tesi/core/src/com/game/screens/MainMenuScreen.java
@@ -12,6 +12,12 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.game.AdventureGame;
 
+/**
+ * 
+ * Classe per il menù principale del gioco
+ *
+ */
+
 public class MainMenuScreen implements Screen
 {
 	public static final int Width = 80;
